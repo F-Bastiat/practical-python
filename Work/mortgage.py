ef488668296eb6f9d13294dd4f43f3a336add410
Mihai <rusnac.m@gmail.com>
2021-11-23 02:51:50 +0200

    full calculations

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,32 +6,48 @@ from dateutil.relativedelta import relativedelta as rd
 
 
 month = dt.strptime('2021-11-03', '%Y-%m-%d')
-principal = 576100.0
+principal = 552253.0
 _principal = principal
 rate = 0.0449
 periods = 360
 insurance_rate = 0.0035
 payment_after_7 = 2681.27
-extra_payment_calendar = [
-    {'from_month': 2, 'to_month': 2, 'extra': 10000},
-    {'from_month': 3, 'to_month': 12, 'extra': 15000},
-    {'from_month': 13, 'to_month': 24, 'extra': 18000},
-    {'from_month': 25, 'to_month': 360, 'extra': 20000}
+extra_payment_cal_m = [
+    {'from_month': 1, 'to_month': 1, 'extra': 5000},
+    {'from_month': 3, 'to_month': 12, 'extra': 10000},
+    {'from_month': 13, 'to_month': 24, 'extra': 10000},
+    {'from_month': 25, 'to_month': 360, 'extra': 12000}
+]
+extra_payment_cal_f = [
+    {'from_month': 1, 'to_month': 1, 'extra': 5000},
+    {'from_month': 2, 'to_month': 2, 'extra': 7000},
+    {'from_month': 3, 'to_month': 12, 'extra': 5000},
+    {'from_month': 13, 'to_month': 24, 'extra': 5000},
+    {'from_month': 25, 'to_month': 360, 'extra': 6000}
 ]
 extra_payment = 0.0
-bonus_calendar = [
-    {'month': 2, 'bonus': 35000},
+bonus_cal_m = [
+    {'month': 2, 'bonus': 55000},
+    {'month': 13, 'bonus': 20000}
+]
+bonus_cal_f = [
     {'month': 9, 'bonus': 30000},
     {'month': 21, 'bonus': 35000},
     {'month': 33, 'bonus': 40000}
 ]
 bonus = 0.0
+total_paid_m = 0.0
+total_paid_f = 0.0
 total_paid = 0.0
+total_rates = 0.0
 period = 0
 
-print (f'{"Nr":>3} {"Mon":>10} {"Rate":>12} {"Extra":>12} {"Bonus":>12} {"Payment":>12} {"Interest":>12} {"Princip.rate":>12} {"Total paid":>12} {"Principal":>12}')
-print('----------------------------------------------------------------------------------------------------------------------')
-print(f'{period:>3} {month:%Y-%m-%d} {0:>12,.2f} {0:>12,.2f} {0:>12,} {0:>12,.2f} {0:>12,.2f} {0:>12,.2f} {total_paid:>12,.2f} {principal:>12,.2f}')
+print(f'{"Nr":>3} {"Mon":>10} {"Rate":>12} {"Extra M":>12} {"Extra F":>12} {"Extra":>12} {"Bonus M":>12} {"Bonus F":>12}', end=" ")
+print(f'{"Bonus":>12} {"Payment":>12} {"Interest":>12} {"Princip.rate":>12} {"Total paid":>12} {"Principal":>12}')
+print('------------------------------------------------------------------------------------------------------------------', end="")
+print('--------------------------------------------------------')
+print(f'{period:>3} {month:%Y-%m-%d} {0:>12,.2f} {0:>12,.2f} {0:>12,.2f} {0:>12,.2f} {0:>12,.2f} {0:>12,.2f}', end=" ")
+print(f'{0:>12,} {0:>12,.2f} {0:>12,.2f} {0:>12,.2f} {total_paid:>12,.2f} {principal:>12,.2f}')
 
 for period in range(periods):
     if principal > 0:
@@ -56,20 +72,39 @@ for period in range(periods):
     else:
         payment = payment_after_7
 
-    extra_payment = max([e['extra'] for e in extra_payment_calendar if period >= e['from_month'] and period <= e['to_month']], default=0)
+    extra_payment_m = max([e['extra'] for e in extra_payment_cal_m if period >= e['from_month'] and period <= e['to_month']], default=0)*1.0
+    extra_payment_f = max([e['extra'] for e in extra_payment_cal_f if period >= e['from_month'] and period <= e['to_month']], default=0)*1.0
+    extra_payment = extra_payment_m + extra_payment_f
     if extra_payment > principal - payment:
         extra_payment = principal * (1+rate/12) - payment
+        extra_payment_m = (principal * (1+rate/12) - payment) / 2
+        extra_payment_f = (principal * (1+rate/12) - payment) / 2
 
-    bonus = max([e['bonus'] for e in bonus_calendar if period == e['month']], default=0)
+    bonus_m = max([e['bonus'] for e in bonus_cal_m if period == e['month']], default=0)*1.0
+    bonus_f = max([e['bonus'] for e in bonus_cal_f if period == e['month']], default=0)*1.0
+    bonus = bonus_m + bonus_f
     if bonus > principal - payment - extra_payment:
         bonus = principal * (1+rate/12) - payment - extra_payment
+        bonus_m = (principal * (1+rate/12) - payment - extra_payment) / 2
+        bonus_f = (principal * (1+rate/12) - payment - extra_payment) / 2
 
     current_payment = payment + extra_payment + bonus
     principal_rate = payment - interest
     principal = principal - principal_rate - extra_payment - bonus
+    total_paid_m = total_paid_m + extra_payment_m + bonus_m
+    total_paid_f = total_paid_f + extra_payment_f + bonus_f + payment
     total_paid = total_paid + current_payment
+    total_rates = total_rates + payment
     
-    print(f'{period:>3} {month:%Y-%m-%d} {payment:>12,.2f} {extra_payment:>12,.2f} {bonus:>12,.2f} {current_payment:>12,.2f} {interest:>12,.2f} {principal_rate:>12,.2f} {total_paid:>12,.2f} {principal:>12,.2f}')
+    print(f'{period:>3} {month:%Y-%m-%d} {payment:>12,.2f} {extra_payment_m:>12,.2f} {extra_payment_f:>12,.2f} {extra_payment:>12,.2f} {bonus_m:>12,.2f}', end=" ")
+    print(f'{bonus_f:>12,.2f} {bonus:>12,.2f} {current_payment:>12,.2f} {interest:>12,.2f} {principal_rate:>12,.2f} {total_paid:>12,.2f} {principal:>12,.2f}')
 
-print('======================================================================================================================')
-print(f'Total paid: {total_paid:>16,.2f}\nCredit cost: {total_paid - _principal:>15,.2f}\nMonths: {period:>17}')
+print('==================================================================================================================================================', end="")
+print('========================')
+print(f'Total paid: {total_paid:>16,.2f}')
+print(f'Credit cost: {total_paid - _principal:>15,.2f}')
+print(f'Months: {period:>17}')
+print(f'Total rates: {total_rates:>15,.2f}')
+print(f'Total paid M: {total_paid_m:>14,.2f} | {total_paid_m / total_paid:.2%}')
+print(f'Total paid F: {total_paid_f:>14,.2f} | {total_paid_f / total_paid:.2%}')
+print(f'Outstanding: {(max(total_paid_m, total_paid_f) - min(total_paid_m, total_paid_f))/2:>15,.2f}')
